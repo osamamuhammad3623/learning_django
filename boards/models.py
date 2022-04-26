0293efe125c38d0a15cd099333d80194a33fa8d1
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-04-27 01:33:33 +0200

    initial commit

---
+++ boards/models.py
@@ -0,0 +1,25 @@
+from email import message
+from django.db import models
+from django.contrib.auth.models import User
+
+class Board(models.Model):
+    board_pk = models.IntegerField(primary_key=True)
+    name = models.CharField(unique=True, max_length=30)
+    desc = models.CharField(unique=False, max_length=150)
+
+
+class Topic(models.Model):
+    topic_pk = models.IntegerField(primary_key=True)
+    subject: models.CharField(max_length=150)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # to store date & time:
+    # time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    post_pk = models.IntegerField(primary_key=True)
+    message = models.CharField(max_length=150)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
---
+++ boards/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
+def home_response(request):
+    # to get Board objects from DB
+    boards = Board.objects.all()
+    names_str = ""
+    for b in boards:
+        names_str += b.name + " "
+    return HttpResponse(f"Available boards: {names_str}")
