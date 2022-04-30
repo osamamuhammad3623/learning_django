3f2884a494666b0b446e755293c3925848a9569f
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-04-30 13:20:11 +0200

    adding home template

--- boards/views.py
+++ boards/views.py
@@ -5,8 +5,5 @@ from .models import Board
 def home_response(request):
     # to get Board objects from DB
     boards = Board.objects.all()
-    
-    names_str = ""
-    for b in boards:
-        names_str += b.name + " "
-    return HttpResponse(f"Available boards: {names_str}")
+
+    return render(request, 'home.html', {'boards': boards})
