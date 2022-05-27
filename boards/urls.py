97f72e6e34b759debd5d70ada1ac3e92d2fac8ed
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-05-27 19:15:04 +0200

    add the url navigation

--- boards/urls.py
+++ boards/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_response, name='home_response'),
+    path('<int:board_id>/',views.board_topics, name="board_topics")
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,18 @@
+from pydoc_data.topics import topics
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Topic
 
 def home_response(request):
     # to get Board objects from DB
     boards = Board.objects.all()
 
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, board_id):
+    selected_board = Board.objects.get(board_pk=board_id)
+    #board_topics = Topic.objects.get(board=selected_board)
+    print(selected_board)
+    
+    return render(request, 'topics.html', {'board': selected_board.name})
